35fb4ab638a6eb8ab290489ff869142e52cc209b
pilevsky <136490233+pilevsky@users.noreply.github.com>
2023-06-18 17:04:18 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-console.log('Hello World');
-console.log(666);
-console.log('Hello' + ' world!');
+console.log(10);
+console.log('Добро '+'пожаловать '+'на курс '+'Дмитрий!');
+//alert('19.06.2023');
+document.querySelector(".one").innerHTML = "Hello World!";
+typeof undefined // undefined
+console.log(typeof undefined);
+typeof 0 // number
+console.log (typeof 0);
+typeof 10n // bigint
+console.log(typeof 10n);
+typeof true // boolean
+console.log(typeof true);
+typeof 'cat' // string
+console.log(typeof 'cat');
+typeof Symbol('id') // symbol
+console.log(typeof Symbol('id'));
