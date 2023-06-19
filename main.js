aa26032141d16289f25c9377e8c4c21c8049c1a9
pilevsky <pilevskiyd@yandex.ru>
2023-06-19 22:30:00 +0600

    Третий коммит

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
-console.log(10);
-console.log('Добро '+'пожаловать '+'на курс '+'Дмитрий!');
-//alert('19.06.2023');
-document.querySelector(".one").innerHTML = "Hello World!";
-typeof undefined // undefined
-console.log(typeof undefined);
-typeof 0 // number
-console.log (typeof 0);
-typeof 10n // bigint
-console.log(typeof 10n);
-typeof true // boolean
-console.log(typeof true);
-typeof 'cat' // string
-console.log(typeof 'cat');
-typeof Symbol('id') // symbol
-console.log(typeof Symbol('id'));
+// console.log(10);
+// console.log('Добро '+'пожаловать '+'на курс '+'Дмитрий!');
+// //alert('19.06.2023');
+// document.getElementById("one").innerHTML = "Hello World!";
+// typeof undefined // undefined
+// console.log(typeof undefined);
+// typeof 0 // number
+// console.log (typeof 0);
+// typeof 10n // bigint
+// console.log(typeof 10n);
+// typeof true // boolean
+// console.log(typeof true);
+// typeof 'cat' // string
+// console.log(typeof 'cat');
+// typeof Symbol('id') // symbol
+// console.log(typeof Symbol('id'));
+
+
+//document.querySelector('one').innerHTML = 666;
+
+//let inp = document.querySelector('.text');
+//let btn = document.querySelector('.check');
+//let div = document.querySelector(div);
+//btn.onclick = function(){
+//   let e = inp.value;
+//    div.innerHTML = e `<h1>${e}</h1>`;
+//};
+
+
+// let dr = prompt ('Когда день рождение?');
+// console.log(dr);
+// if(dr == 19) {
+//     alert('Поздравляю!!!');
+// } else {
+//     alert('Приходи Завтра!!!');
+// };
+
+//if(Условие){
+//    действие выполнится если условие истинно;
+//}
+
+let number = prompt('Введите число:');
+if(number > 100) {
+    alert (number)}
+    else {
+    console.log(number)
+    }
